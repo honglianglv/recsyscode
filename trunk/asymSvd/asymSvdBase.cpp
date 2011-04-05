13d009cfbcab0bbf7ca98ee61622559da418a6c1
honglianglv <honglianglv@1f217404-4e1e-d0d8-b025-432a2f2a53b5>
2011-04-05 09:48:05 +0000

    add asymmetric svd model, this model is decribed in the page 5 of koren's SIGKDD'08 paper.

---
+++ trunk/asymSvd/asymSvdBase.cpp
@@ -0,0 +1,229 @@
+/* This file is Copyright (C) 2011 Lv Hongliang. All Rights Reserved.
+ * please maitain the copyright information completely when you redistribute the code.
+ * 
+ * If there are some bugs, please contact me via email honglianglv@gmail.com or submit the bugs 
+ * in the google code project http://code.google.com/p/recsyscode/issues/list
+ * 
+ * my blog: http://lifecrunch.biz
+ * my twitter: http://twitter.com/honglianglv
+ * my google profile:https://profiles.google.com/honglianglv/about?hl=en
+ *
+ * It is free software; you can redistribute it and/or modify it under 
+ * the license GPLV3.
+ *
+ * asymSvdBase.cpp contains some common functions of asymmetric-svd model.
+ * this model is decribed in the page 5 of koren's SIGKDD'08 paper.
+ */
+#ifndef ASYMSVD_ASYMSVDBASE_CPP_
+#define ASYMSVD_ASYMSVDBASE_CPP_
+namespace svd{
+  	//use some global variables，store the parameter bu, bi, p, q,y
+	double bu[USER_NUM+1] = {0};       // the user bias in the baseline predictor
+    double bi[ITEM_NUM+1] = {0};       // the item bias in the baseline predictor
+    float buBase[USER_NUM+1] = {0};
+    float biBase[ITEM_NUM+1] = {0};       //baseline预测器中的用户偏置和item偏置
+    
+    int buNum[USER_NUM+1] = {0};       //用户u打分的item总数， num of user ratings
+    int biNum[ITEM_NUM+1] = {0};       //打过item i分的用户总数 num of item ratings
+    
+    double pu[USER_NUM+1][K_NUM+1] = {0};   //用于存储中间变量puk       store complete character of user
+    double q[ITEM_NUM+1][K_NUM+1] = {0};   //用于item的属性描述q       item character Matrix
+    double  x[ITEM_NUM+1][K_NUM+1];          //explicit weight x
+    double y[ITEM_NUM+1][K_NUM+1] = {0};   //implicit weight y
+    float mean = 0;                         //全局的平均值             mean of all ratings
+    
+    vector < vector<rateNode> > rateMatrix(USER_NUM+1);   //store training set
+    vector<testSetNode> probeRow;                            //store test set
+    
+	//initialize the bias bu and bi, the method in the page 2 of koren's TKDD'09 paper
+	void initialBais()
+	{
+		using namespace svd;
+	    int i,j;
+	    for(i = 1; i < USER_NUM+1; ++i){
+	    	int vSize = rateMatrix[i].size();
+			for(j=0; j < vSize; ++j) {
+				bi[rateMatrix[i][j].item] += (rateMatrix[i][j].rate - mean);
+				biNum[rateMatrix[i][j].item] += 1;
+			}			
+	    }
+	    
+	    for(i = 1; i < ITEM_NUM+1; ++i) {
+	    	if(biNum[i] >=1)bi[i] = bi[i]/(biNum[i]+25);
+	    	else bi[i] = 0.0;
+	    	biBase[i] = bi[i];
+	    }
+	   
+	     for(i = 1; i < USER_NUM+1; ++i){
+	    	int vSize = rateMatrix[i].size();
+			for(j=0; j < vSize; ++j) {
+				bu[i] += (rateMatrix[i][j].rate - mean - bi[rateMatrix[i][j].item]);
+				buNum[i] += 1;
+			}			
+	    }
+	    for(i = 1; i < USER_NUM+1; ++i) {
+	    	if(buNum[i]>=1)bu[i] = bu[i]/(buNum[i]+10);
+	    	else bu[i] = 0.0;
+	    	buBase[i] = bu[i];
+	    }
+	}
+	
+	//intialize the matrix of explicit character(X), the matrix of item character(Q) and implicit matrix Y
+	void initialPQ(int itemNum, int userNum,int dim)
+	{
+		using namespace svd;
+		int i;
+		//@TODO 不知道是否能针对初始化的过程做一些优化
+		for(int i = 1; i < itemNum+1; ++i){
+	        setRand(x[i],dim,0); 
+	        setRand(q[i],dim,0);
+	        setRand(y[i],dim,0); 
+	    }
+	}
+	
+	void model(int dim, float  alpha1, float alpha2, float beta1, float beta2,
+				 int maxStep=60,double slowRate=1,bool isUpdateBias=true)
+    {
+        cout << "begin initialization: " << endl;
+        loadRating(TRAINING_SET,rateMatrix,RATE_SP);  //load training set
+        loadProbe(PROBE_SET,probeRow,RATE_SP);   //load test set
+        mean = setMeanRating(USER_NUM,rateMatrix); //calculate the mean
+        int i,u,j,k;
+        
+        srand((unsigned)time(0)); 
+        //initialBais(); //initialize bu and bi
+        
+        initialPQ(ITEM_NUM, USER_NUM,K_NUM); //intialize the matrix of user character(P) and the matrix of item character(Q) 
+        
+        //initialize pu
+        cout <<"begin compute first pu: " << endl;
+        for( u = 1; u < USER_NUM+1; ++u) {   //循环处理每一个用户 
+           	int RuNum = rateMatrix[u].size(); //用户u打过分的item数目
+           	float sqrtRuNum = 0.0;
+           	if(RuNum>1) sqrtRuNum = (1.0/sqrt(RuNum));
+           	
+           	//求出pu
+           	for( k=1; k< K_NUM+1; ++k) {
+           		float sumx = 0.0;
+           		float sumy = 0.0;
+           		
+           		for(j=0; j < RuNum; ++j) {
+           			int itemI = rateMatrix[u][j].item;
+           			short rui =  rateMatrix[u][j].rate;
+           			float bui = mean + buBase[u] + biBase[itemI];
+           			sumx += (rui-bui) * x[itemI][k];
+           			sumy += y[itemI][k];
+           		}
+           		pu[u][k] = sqrtRuNum*(sumx + sumy);
+           	}
+        }
+        cout <<"end compute first pu! " << endl;
+        
+        cout <<"initialization end!"<<endl<< "begin iteration: " << endl;
+        float pui = 0.0 ; // 预测的u对i的打分
+        double preRmse = 1000000000000.0; //用于记录上一个rmse，作为终止条件的一种，如果rmse上升了，则停止
+        double nowRmse = 0.0;
+        cout <<"begin testRMSEProbe(): " << endl;
+        RMSEProbe(probeRow,K_NUM);
+        //main loop
+        for(int step = 0; step < maxStep; ++step){  //只迭代60次
+            long double rmse = 0.0;
+            int n = 0;
+            for( u = 1; u < USER_NUM+1; ++u) {   //循环处理每一个用户    
+                int RuNum = rateMatrix[u].size(); //用户u打过分的item数目
+                float sqrtRuNum = 0.0;
+                if(RuNum>1) sqrtRuNum = (1.0/sqrt(RuNum));
+                
+                
+                //calculate pu
+               	for( k=1; k< K_NUM+1; ++k) {
+               		float sumx = 0.0;
+               		float sumy = 0.0;
+               		
+               		for(j=0; j < RuNum; ++j) {
+               			int itemI = rateMatrix[u][j].item;
+               			short rui =  rateMatrix[u][j].rate;
+               			float bui = mean + buBase[u] + biBase[itemI];
+               			sumx += (rui-bui) * x[itemI][k];
+               			sumy += y[itemI][k];
+               		}
+               		pu[u][k] = sqrtRuNum*(sumx + sumy);
+               	}
+               	
+                float sum[K_NUM+1] = {0};   //用于存储中间变量sum
+                   
+                //迭代处理
+                for(i=0; i < RuNum; ++i) {// 循环处理u打分过的每一个item
+                    int itemI = rateMatrix[u][i].item;
+                    short rui = rateMatrix[u][i].rate; //实际的打分 real rate
+                    //double bui = mean + bu[u] + bi[itemI];
+                    double bui = mean + buBase[u] + biBase[itemI];
+                    pui = predictRate(u,itemI,dim);  //predict rate
+                    
+                    float eui = rui - pui;
+                    
+                    if( isnan(eui) ) {// fabs(eui) >= 4.2 || 
+                        cout<<u<<'\t'<<i<<'\t'<<pui<<'\t'<<rui<<"    "<<bu[u]<<"    "<<bi[itemI]<<"    "<<mean<<endl;
+                        exit(1);
+                    }
+                    rmse += eui * eui; ++n;
+                    if(n % 10000000 == 0)cout<<"step:"<<step<<"    n:"<<n<<" dealed!"<<endl;
+                    
+                    if(isUpdateBias) {
+                    	bu[u] += alpha1 * (eui - beta1 * bu[u]);
+                    	bi[itemI] += alpha1 * (eui - beta1 * bi[itemI]);
+                    }
+                    
+                    for( k=1; k< K_NUM+1; ++k) {
+	               		sum[k] = sum[k]+ eui*q[itemI][k];
+	               		q[itemI][k] += alpha2 * (eui*pu[u][k] - beta2*q[itemI][k]);
+	               	}
+                }
+                
+                for(i=0; i < RuNum; ++i) {// 循环处理u打分过的每一个item
+	                int itemI = rateMatrix[u][i].item;
+                	short rui = rateMatrix[u][i].rate; //实际的打分
+	               	for( k=1; k< K_NUM+1; ++k) {
+	               		
+						x[itemI][k] += alpha2 * (sqrtRuNum*sum[k]*(rui-mean-buBase[u]-biBase[itemI]) - beta2*x[itemI][k]);
+	               		y[itemI][k] += alpha2 * (sqrtRuNum*sum[k] - beta2*y[itemI][k]);
+						/*
+						x[itemI][k] += alpha * (eui*sqrtRuNum*q[itemI][k]*(rui-bui) - beta*x[itemI][k]);
+	               		y[itemI][k] += alpha * (eui*sqrtRuNum*q[itemI][k] - beta*y[itemI][k]);
+						*/
+	               	}
+                } 
+            }
+            nowRmse =  sqrt( rmse / n);
+            
+            if( nowRmse >= preRmse && step >= 3) break; //如果rmse已经开始上升了，则跳出循环
+            else
+                preRmse = nowRmse;
+            cout << step << "\t" << nowRmse <<'\t'<< preRmse<<"     n:"<<n<<endl;
+            RMSEProbe(probeRow,K_NUM);;  // check test set rmse
+            
+            alpha1 *= slowRate;    //逐步减小学习速率
+            alpha2 *= slowRate;
+        }
+        RMSEProbe(probeRow,K_NUM);  // 检查测试集情况
+        return;
+    }
+};
+
+/**
+ * predict the rate
+ */
+float predictRate(int user, int item,int dim)
+{
+	using namespace svd;
+    int RuNum = rateMatrix[user].size(); //用户u打过分的item数目
+    double ret; 
+    if(RuNum >= 1) {
+        ret = mean + bu[user] + bi[item] +  dot(pu[user],q[item],dim);
+    }
+    else ret  = mean+bu[user] + bi[item];
+    if(ret < 1.0) ret = 1;
+    if(ret > 5.0) ret = 5;
+    return ret;
+}
+#endif // ASYMSVD_ASYMSVDBASE_CPP_ 
