7b9a526c0032eed89592ff83860c75dd76781a34
honglianglv <honglianglv@1f217404-4e1e-d0d8-b025-432a2f2a53b5>
2011-04-05 08:51:03 +0000

    add svd++ model, the algorithm is described in the paper of Koren in SIGKDD'08

--- svdplusplus/svdplusplus_ml.cpp
+++ svdplusplus/svdplusplus_ml.cpp
@@ -11,8 +11,8 @@
  * It is free software; you can redistribute it and/or modify it under 
  * the license GPLV3.
  *
- * 本程序的目的是实现koren在SIGKDD'08论文中的方法，svd model, the dataset is movielens
- * The purpose of this program is to implement the in the SVD method of koren's SIGKDD'08 paper,
+ * 本程序的目的是实现koren在SIGKDD'08论文中的SVD++方法, 使用的数据是movielens的数据
+ * The purpose of this program is to implement the SVD++ method of koren's SIGKDD'08 paper,
  * using the movielens dataset.
  *
  */
@@ -23,10 +23,10 @@
 #define USER_NUM 943 //10K:943 1M:6040
 #define ITEM_NUM 1682 //10K:1682 1M:3900
 #define K_NUM  50     //dimension
-#define TRAINING_SET "../dataset/movielens/u1.test" //training set
-#define PROBE_SET "../dataset/movielens/u1.base"  //test set
+#define TRAINING_SET "../dataset/movielens/ua.test" //training set
+#define PROBE_SET "../dataset/movielens/ua.base"  //test set
 #define RATE_SP "	"  //rate Separator
-#include "./svdBase.cpp"
+#include "./svdplusplusBase.cpp"
 
 int main(int argc, char ** argv)
 {
@@ -49,7 +49,7 @@ int main(int argc, char ** argv)
     	//cout << alpha << endl;
     	//void model(int dim, float  alpha1, float alpha2, float beta1, float beta2,
 		//		 int maxStep=60,double slowRate=1,bool isUpdateBias=true)
-    	svd::model(K_NUM,alpha1,alpha2,beta1,beta2,60,0.99,true);	
+    	svd::model(K_NUM,alpha1,alpha2,beta1,beta2,60,1,false);	
     }
     end = time(NULL);
     duration = (end-start);
